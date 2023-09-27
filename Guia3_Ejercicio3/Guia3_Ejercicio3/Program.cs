26e7aae463370414d8b6c97e2dc600d1c3163b40
David Gómez <117203802+davidsgomez@users.noreply.github.com>
2023-09-27 12:18:52 -0600

    Add files via upload

---
+++ Guia3_Ejercicio3/Guia3_Ejercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public class demo
+{
+    public static void Main()
+    {
+        double[] numbers  = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        double exponente = 2;
+        double resultado = 0;
+
+        foreach (double number in numbers) 
+        {
+            double potencia = Math.Pow(number, exponente);
+            resultado += potencia;
+        }
+
+        Console.WriteLine("La acumulacion de los cuadrados de los primeros 20 dígitos es = " + resultado);
+
+    }
+}
---
+++ Guia3_Ejercicio4/Guia3_Ejercicio4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class demo
+{
+    public static void Main()
+    {
+        int a, b, c;
+        double x1 = 0, x2 = 0;
+
+        Console.WriteLine("Escribe el valor A: ");
+        a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Escribe el valor B: ");
+        b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Escribe el valor C: ");
+        c = int.Parse(Console.ReadLine());
+
+        double discriminante = Math.Pow(b, 2) - (4 * a * c);
+
+        if (discriminante < 0)
+        {
+            Console.WriteLine("La ecuación no tiene soluciones reales porque el discriminante es negativo.");
+        }
+        else
+        {
+            x1 = ((-1 * b) + Math.Sqrt(discriminante)) / (2 * a);
+            x2 = ((-1 * b) - Math.Sqrt(discriminante)) / (2 * a);
+
+            Console.Write("El valor de x1 es: {0}", x1);
+            Console.Write("\nEl valor de x2 es: {0}", x2);
+        }
+    }
+}
