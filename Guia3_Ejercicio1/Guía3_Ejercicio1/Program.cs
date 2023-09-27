be93e5711598adb0a61053a1e98e61913b06ebdb
David Gómez <117203802+davidsgomez@users.noreply.github.com>
2023-09-27 12:17:32 -0600

    Add files via upload

---
+++ Guia3_Ejercicio1/Guía3_Ejercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class demo
+{
+    public static void Main()
+    {
+
+        double PI = Math.PI;
+        double truncate = Math.Truncate(PI);
+
+        Console.WriteLine("The trucate value of " + truncate);
+        Console.WriteLine("Redondeado hacia arriba = " + Math.Ceiling(PI));
+        Console.WriteLine("Redondeado hacia abajo = " + Math.Floor(PI));
+
+    }
+}
---
+++ Guia3_Ejercicio2/Guia3_Ejercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+public class demo
+{
+    public static void Main()
+    {
+
+        double euler = Math.E;
+        double redonddecimal = Math.Round(euler, 10);
+
+        Console.WriteLine("Valor de la constante Euler = " + euler);
+        Console.WriteLine("Redondeado solo 10 posiciones = " + redonddecimal);
+
+    }
+}
